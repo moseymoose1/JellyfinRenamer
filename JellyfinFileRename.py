9a49ba2499d51d9958cc529a2acbbb5c6e04cd4f
moseymoose1 <118142576+moseymoose1@users.noreply.github.com>
2023-04-26 00:41:15 +1000

    Add files via upload

--- JellyfinFileRename.py
+++ JellyfinFileRename.py
@@ -5,22 +5,34 @@ import re
 
 def extract_episode_info(filename):
     # match the pattern of season number and episode number
-    pattern = r"S(\d{2})E(\d{2})"
+    pattern = r"S(\d{2})E(\d{2})[E-]?(\d{2})?"
     match = re.search(pattern, filename, re.IGNORECASE)
 
     if match:
         season_number = match.group(1)
         episode_number = match.group(2)
-        return season_number, episode_number
+        if match.group(3):
+            episode_number += f"-{match.group(3)}"
     else:
-        return None, None
+        season_number = None
+        episode_number = None
+
+    # check if the filename contains part information
+    part_match = re.search(r"(part|pt)\.? ?(\d+|one)", filename, re.IGNORECASE)
+    if part_match:
+        part_number = part_match.group(2)
+        part_suffix = f" Part{part_number}"
+    else:
+        part_suffix = ""
+
+    return season_number, episode_number, part_suffix
 
 
 def is_video_file(filename):
     # check if file is a video file
     video_extensions = {".mp4", ".avi", ".mkv", ".wmv", ".flv", ".m4v"}
     extension = os.path.splitext(filename)[1].lower()
-    return os.path.isfile(filename) and extension in video_extensions
+    return extension in video_extensions
 
 
 def validate_tags(tags_input):
@@ -51,28 +63,39 @@ def main():
     # get a list of all files in the specified folder
     files = os.listdir(folder_path)
 
-    for filename in [f for f in files if is_video_file(os.path.join(folder_path, f))]:
-        season_number, episode_number = extract_episode_info(filename)
+    # generator expression to filter video files
+    video_files = (f for f in files if is_video_file(os.path.join(folder_path, f)))
+
+    for file in video_files:
+        old_path = os.path.join(folder_path, file)
+
+        # extract episode information from file name
+        season_number, episode_number, part_suffix = extract_episode_info(file)
 
         if season_number and episode_number:
-            # create new filename with season number, episode number, and tags
-            new_filename = f"Episode S{season_number}E{episode_number}{tags}.mp4"
+            if "-" in episode_number:
+                new_file_name = f"Episode S{season_number}E{episode_number}{part_suffix}{tags}{os.path.splitext(file)[1]}"
+            else:
+                new_file_name = f"Episode S{season_number}E{episode_number}{part_suffix}{tags}{os.path.splitext(file)[1]}"
+            new_path = os.path.join(folder_path, new_file_name)
 
-            # create new file path
-            new_path = os.path.join(folder_path, new_filename)
-
-            # rename the file
-            os.rename(os.path.join(folder_path, filename), new_path)
-
-            # print success message
-            print(f"{filename} renamed to {new_filename}")
+            # rename file
+            os.rename(old_path, new_path)
+            print(f"Renamed '{file}' to '{new_file_name}'")
         else:
-            # print error message
-            print(f"{filename} does not match the pattern SXXEXX and was not renamed")
+            print(f"Could not extract episode information from '{file}'")
 
-    # print completion message
-    print("All files completed.")
+    print("Finished renaming files.")
 
 
 if __name__ == "__main__":
     main()
+    
+              # ask user if they want to rename another folder
+    another_folder = input("Would you like to rename another folder? (Enter 'y' or 'n') ")
+    while another_folder.lower() == "y":
+        main()
+        another_folder = input("Would you like to rename another folder? (Enter 'y' or 'n') ")
+    else:
+        sys.exit(0)
+        
